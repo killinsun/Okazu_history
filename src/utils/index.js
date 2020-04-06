7f4d3a64fe78e08dcd0a40fdc56d9b92c9e1bafd
killinsun <noneck.0311@gmail.com>
2020-04-07 07:58:58 +0900

    modified lastDate formatting
    from original Firebase time stamp formatting to slash based formatting like a yyyy/MM/dd by date-fns

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,16 @@
+import { format } from 'date-fns'
+
 export default {
   transformRecipieObject (recipiesFromApi) {
     if (recipiesFromApi === undefined || recipiesFromApi.length === 0) return null
     const transformedRecipies = []
     recipiesFromApi.forEach((recipie) => {
+      const formattedLastDate = format(recipie.data().lastDate.toDate(), 'yyyy/MM/dd')
       transformedRecipies.push({
         id: recipie.id,
         name: recipie.data().name,
         count: recipie.data().count,
-        lastDate: recipie.data().lastDate.toDate(),
+        lastDate: formattedLastDate,
         deleted: recipie.data().deleted,
         favorited: recipie.data().favorited,
         imageSrc: recipie.data().imageSrc
